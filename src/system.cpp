2371196f1867b297245232aad98599658365e36e
Robotawi <mohamed.s.raessa@gmail.com>
2022-10-14 22:22:56 +0900

    Return parsed kernel, os, and memory util

--- src/system.cpp
+++ src/system.cpp
@@ -7,6 +7,7 @@
 #include "process.h"
 #include "processor.h"
 #include "system.h"
+#include "linux_parser.h"
 
 using std::set;
 using std::size_t;
@@ -15,22 +16,22 @@ using std::vector;
 
 //What is the relationship between this class, and the LinuxParser namespace functions?\
 For example, this class wants to get the uptime of the system with its UpTime() function.\
-This UpTime() here can call the function with the same name in the LinuxParser namespace to get this data. 
+This UpTime() here can call the function with the same name in the LinuxParser namespace to get this data.
 
 // TODO: Return the system's CPU
-Processor& System::Cpu() { return cpu_; }
+Processor &System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process> &System::Processes() { return processes_; }
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
