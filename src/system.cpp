df57c3a75cdb28b9d680ce64f058e0d6a788ac53
Robotawi <mohamed.s.raessa@gmail.com>
2022-10-15 10:53:44 +0900

    Return parsed total and running processes

--- src/system.cpp
+++ src/system.cpp
@@ -34,10 +34,10 @@ float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
 // TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
 // TODO: Return the number of seconds since the system started running
 /*Properly format the uptime. Refer to the comments mentioned in format. cpp for formatting the uptime.*/
