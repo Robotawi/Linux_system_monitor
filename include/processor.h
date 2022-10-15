2f6e303a6b236d3bfe40d0a24f448c041e6531d8
Robotawi <mohamed.s.raessa@gmail.com>
2022-10-15 20:01:59 +0900

    Add variables to calc inst cpu util

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,21 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
-class Processor {
- public:
-  float Utilization();  // TODO: See src/processor.cpp
+#include<array>
 
-  // TODO: Declare any necessary private members
- private:
+class Processor
+{
+public:
+    Processor() : prevIdleTime(0.0), prevTotalTime(0.0), deltaIdleTime(0.0), deltaTotalTime(0.0), cpuUtil(0.0) {}
+    float Utilization();
+
+private:
+    std::array<float, 2> cpuTimes;
+    float prevIdleTime;
+    float prevTotalTime;
+    float deltaIdleTime;
+    float deltaTotalTime;
+    float cpuUtil;
 };
 
 #endif
