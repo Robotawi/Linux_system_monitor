5a277d2afae9c854d22f47c5519a2e83f0c4e892
Robotawi <mohamed.s.raessa@gmail.com>
2022-10-15 10:02:59 +0900

    Add function to add zero to time if needed

--- include/format.h
+++ include/format.h
@@ -3,8 +3,11 @@
 
 #include <string>
 
-namespace Format {
-std::string ElapsedTime(long times);  // TODO: See src/format.cpp
-};                                    // namespace Format
+namespace Format
+{
+    std::string ElapsedTime(long times);
+
+    std::string FormatZero(long times);
+};
 
 #endif
