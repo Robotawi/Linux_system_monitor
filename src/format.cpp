e78580e3390f0e59bbfe2360dcd894868a79181d
Robotawi <mohamed.s.raessa@gmail.com>
2022-10-15 10:04:29 +0900

    Format seconds into HH:MM:SS

--- src/format.cpp
+++ src/format.cpp
@@ -3,9 +3,20 @@
 #include "format.h"
 
 using std::string;
+using std::to_string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::FormatZero(long times)
+{
+    return times < 10 ? "0" + to_string(times) : to_string(times);
+}
+
+string Format::ElapsedTime(long seconds)
+{
+    int hours = seconds / 3600;
+    seconds %= 3600;
+
+    int mins = seconds / 60;
+    seconds %= 60;
+
+    return FormatZero(hours) + ":" + FormatZero(mins) + ":" + FormatZero(seconds);
+}
