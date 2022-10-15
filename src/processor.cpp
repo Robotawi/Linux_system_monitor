924e0dbabc7a02c311c7690f02f14fc5cf0bafc4
Robotawi <mohamed.s.raessa@gmail.com>
2022-10-15 20:06:55 +0900

    Calculate instant CPU utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,25 @@
 #include "processor.h"
+#include "linux_parser.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization()
+{   
+    // Return instant CPU utilization
+    // cpuTimes.at(0) is the total cpu time 
+    // cpuTimes.at(1) is the idle cpu time
+
+    // Reference: see this https://rosettacode.org/wiki/Linux_CPU_utilization
+    
+    cpuTimes = LinuxParser::CpuUtilization();
+    deltaTotalTime = cpuTimes.at(0) - prevTotalTime; 
+    deltaIdleTime = cpuTimes.at(1) - prevIdleTime; 
+
+    cpuUtil = 1 - (deltaIdleTime/deltaTotalTime); 
+
+    prevTotalTime = cpuTimes.at(0);
+    prevIdleTime = cpuTimes.at(1);
+
+    return cpuUtil;
+
+    // For the aggregate CPU utilization,\
+    cpuUtil = 1 - (cpuTimes.at(1)/cpuTimes.at(0)) would be enough
+}
